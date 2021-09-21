bcd1b9b84cddf15600bcea6f14176c1dbe1bad26
Life <33139576+dust-life@users.noreply.github.com>
2021-09-21 22:39:15 +0800

    Update Utilities.cs

--- ADCollector/Utilities.cs
+++ ADCollector/Utilities.cs
@@ -990,30 +990,7 @@ namespace ADCollector
 
 
 
-        public static void RunAs(string domain, string username, string password, Action action)
-        {
-            using (var accessToken = GetUserAccessToken(domain, username, password))
-            {
-                WindowsIdentity.RunImpersonated(accessToken, action);
-            }
-        }
 
-
-
-
-        internal static SafeAccessTokenHandle GetUserAccessToken(string domain, string username, string password)
-        {
-            const int LOGON32_PROVIDER_DEFAULT = 0;
-            const int LOGON32_LOGON_NETONLY = 9;
-
-            bool isLogonSuccessful = LogonUser(username, domain, password, LOGON32_LOGON_NETONLY, LOGON32_PROVIDER_DEFAULT, out var safeAccessTokenHandle);
-            if (!isLogonSuccessful)
-            {
-                throw new Win32Exception(Marshal.GetLastWin32Error());
-            }
-
-            return safeAccessTokenHandle;
-        }
 
 
 
